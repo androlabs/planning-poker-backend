0640804b3e9b44711e12099c3274e6c1a8aa1102
gabriel-roque <gabrielrms.dll@gmail.com>
2022-05-14 23:40:07 -0300

    fix: build and dev watch mode

--- src/main/config/module-alias.ts
+++ src/main/config/module-alias.ts
@@ -2,8 +2,8 @@ import { addAliases } from 'module-alias';
 import { resolve } from 'path';
 
 addAliases({
-  '@application': resolve('dist'),
-  '@domain': resolve('dist'),
-  '@infra': resolve('dist'),
-  '@main': resolve('dist'),
+  '@application': resolve('dist/application'),
+  '@domain': resolve('dist/domain'),
+  '@infra': resolve('dist/infra'),
+  '@main': resolve('dist/main'),
 });
